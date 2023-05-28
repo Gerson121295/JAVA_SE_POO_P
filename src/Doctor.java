215386c8044bf856ba2e518ce9f05a614768e749
Gerson121295 <Gerson21millones@gmail.com>
2023-05-28 11:11:42 -0600

    1 -Creacion de project y declarando e instanciando un objeto

---
+++ src/Doctor.java
@@ -0,0 +1,15 @@
+public class Doctor {
+    /** Atributos
+     *
+     */
+    int id;
+    String name;
+    String speciality;
+
+    /**
+     * Comportamientos
+     */
+    public void showName(){ //Nombre del doctor
+        System.out.println(name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //Creacion del objeto
+         //Doctor= nombre de la clase
+        // myDoctor = Nombre del obejto
+        //New Doctor(); = la instanciacion - creando el objeto
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Alejandro Rodriguez"; //asignando el nombre al doctor
+        myDoctor.showName();
+    }
+}
