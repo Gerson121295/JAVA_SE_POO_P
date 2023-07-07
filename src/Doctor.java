ee740fff4ff1401c22c751c55dac3bd655ecb5cc
Gerson121295 <Gerson21millones@gmail.com>
2023-07-07 11:30:20 -0600

    2 - Creacion de elementos estaticos. Se creo un menu(UIMenu) y se importa la clase UIMenu en el main para usarse

--- src/Doctor.java
+++ src/Doctor.java
@@ -2,9 +2,18 @@ public class Doctor {
     /** Atributos
      *
      */
-    int id;
+   static int id = 0; // Autoincrement la variable debe ser estatica para poder llevar el control de autoincremento.
     String name;
     String speciality;
+
+    Doctor(){ //opcional: al llamar el metodo Doctor() se ejecuta lo sig.
+        System.out.println("COnstruyendo el objeto Doctor");
+        id++; // aumenta el id del doctor al crear un constructor.
+    }
+
+    Doctor(String name){ //Opcional agregarle un parametro
+        System.out.println("El nombre del doctor asignado es: "+name);
+    }
 
     /**
      * Comportamientos
@@ -12,4 +21,11 @@ public class Doctor {
     public void showName(){ //Nombre del doctor
         System.out.println(name);
     }
+    public void showId(){
+        System.out.println("ID Doctor: "+ id);
+    }
+
 }
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+import static ui.UIMenu.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,8 +9,32 @@ public class Main {
          //Doctor= nombre de la clase
         // myDoctor = Nombre del obejto
         //New Doctor(); = la instanciacion - creando el objeto
+        //Cuando queremos llamar a los metodos siempre se usa parentesis
+        //new Doctor(); //El metodo constructor siempre va despues de la palabra reservada new,
+        //y va a usar el nombre de la clase seguido de parentesis
+
         Doctor myDoctor = new Doctor();
+        //Doctor myDoctor = new Doctor("Gerson Ep"); // otra forma se envia un argumento name
         myDoctor.name = "Alejandro Rodriguez"; //asignando el nombre al doctor
         myDoctor.showName();
+        myDoctor.showId(); // obtener id forma 1
+        System.out.println(Doctor.id); // obtener id forma 2
+
+        Doctor.id++; //Aumtamos en 1 para eso la variable debe ser estatica.
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctorAnn.name = "Ann";
+        myDoctorAnn.showName();
+        myDoctorAnn.showId(); // obtener id forma 1
+        System.out.println(Doctor.id); // obtener id forma 2
+
+    // LLamado del metodo showMenu de la clase ui.UIMenu otra forma es importar la
+        // clase y solo escribir el metodo showMenu();
+         //UIMenu.showMenu(); //Sin Importar la clase, La clase no estaba dentro de la carpeta ui.
+
+        showMenu();
+
     }
+
+
 }
